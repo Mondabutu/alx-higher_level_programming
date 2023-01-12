9b6d511e6f04a417224c75dc0968a7666f8d2a1f
Mondabutu <mondabutu@gmail.com>
2023-01-12 01:03:44 +0100

    More data structures

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    new_mat = []
+    if len(matrix) == 0:
+        return new_mat
+
+    new_mat = [[i*i for i in j] for j in matrix]
+    return new_mat
