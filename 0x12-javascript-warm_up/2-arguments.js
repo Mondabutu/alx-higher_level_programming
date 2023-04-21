6b35c6cfb4c26eac75e46c6711d1048ca03b71d3
Mondabutu <mondabutu@gmail.com>
2023-04-21 22:06:48 +0100

    Javascript warm up

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
 const argc = process.argv.length;
-if (argc === 2) {
+
+if (argc > 2) {
+  console.log('Argument' + (argc > 3 ? 's' : '') + ' found');
+} else {
   console.log('No argument');
-} else if (argc === 3) {
-  console.log('Argument found');
-} else if (argc > 3) {
-  console.log('Arguments found');
 }
